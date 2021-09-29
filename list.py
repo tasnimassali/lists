545f6ec5b664afdced08fc59461bfeb9cc60756f
tasnimassali <36279243+tasnimassali@users.noreply.github.com>
2021-09-29 15:42:51 +0100

    Add files via upload

---
+++ list.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Creat list 
+# empty list
+my_list = []
+
+# list of integers
+my_list = [1, 2, 3]
+
+# list with mixed data types
+my_list = [1, "Hello", 3.4]
+
+# nested list
+my_list = ["mouse", [8, 4, 6], ['a']]
+
+# Correcting mistake values in a list
+odd = [2, 4, 6, 8]
+
+# change the 1st item    
+odd[0] = 1            
+
+print(odd)
+
+# change 2nd to 4th items
+odd[1:4] = [3, 5, 7]  
+
+print(odd) 
+
+# Appending and Extending lists in Python
+odd = [1, 3, 5]
+
+odd.append(7)
+
+print(odd)
+
+odd.extend([9, 11, 13])
+
+print(odd)
+# Concatenating and repeating lists
+odd = [1, 3, 5]
+
+print(odd + [9, 7, 5])
+
+print(["re"] * 3)
+# Deleting list items
+my_list = ['p', 'r', 'o', 'b', 'l', 'e', 'm']
+
+# delete one item
+del my_list[2]
+
+print(my_list)
+
+# delete multiple items
+del my_list[1:5]
+
+print(my_list)
+
+# delete entire list
+del my_list
+
+# Error: List not defined
+print(my_list)
+
